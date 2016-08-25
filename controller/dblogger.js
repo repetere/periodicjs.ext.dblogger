322f05bbc55716391f527a49df16744781cd2b75
Yaw Joseph Etse <yaw.etse@gmail.com>
2016-08-25 10:01:00 -0400

    updated logger search functions

--- controller/dblogger.js
+++ controller/dblogger.js
@@ -13,46 +13,46 @@ let dbloggerSettings;
 let mongooseLogger;
 
 module.exports = function(resources) {
-    appSettings = resources.settings;
-    appenvironment = appSettings.application.environment;
-    CoreController = resources.core.controller;
-    CoreUtilities = resources.core.utilities;
-    dbloggerSettings = resources.app.controller.extension.dblogger.settings;
-    mongooseLogger = resources.app.controller.extension.dblogger.settings.getDBConnection().model(dbloggerSettings.model_name);
-    logger = resources.logger;
-    let cronSettings = {
-        model_name: 'dblogger',
-        model: mongooseLogger,
-        controllerOptions: {
-            model: mongooseLogger
-        },
-        searchfields: ['level', 'msg', 'meta', 'hostname'],
-        use_admin_menu: true,
-        use_plural_view_names: true,
-        // load_model_population: 'asset',
-        // load_multiple_model_population: 'asset',
-        use_full_data: true,
-        extname: 'periodicjs.ext.dblogger'
-    };
-    let override = {
-        // create_item: [setCronFilePath, assetController.multiupload, assetController.create_assets_from_files, createCrons],
-        // create_index: [cron_create_index],
-        // delete_item: [deleteCron],
-        update_item: [
-            function(req, res, next) {
-                req.body.runtime_options = JSON.parse(req.body.runtime_options || '{}');
-                next();
-            },
-            CoreController.save_revision,
-            CoreController.controller_update(cronSettings)
-        ]
-    };
-    cronSettings.override = override;
-    let dbloggerController = CoreController.controller_routes(cronSettings);
+  appSettings = resources.settings;
+  appenvironment = appSettings.application.environment;
+  CoreController = resources.core.controller;
+  CoreUtilities = resources.core.utilities;
+  dbloggerSettings = resources.app.controller.extension.dblogger.settings;
+  mongooseLogger = resources.app.controller.extension.dblogger.settings.getDBConnection().model(dbloggerSettings.model_name);
+  logger = resources.logger;
+  let cronSettings = {
+    model_name: 'dblogger',
+    model: mongooseLogger,
+    controllerOptions: {
+      model: mongooseLogger
+    },
+    searchfields: ['level', 'msg', 'meta', 'hostname', 'meta.ipinfo.user', 'meta.ipinfo.x-forwarded-for', 'meta.ipinfo.remoteAddress', 'meta.ipinfo.referer', 'meta.ipinfo.originalUrl', 'meta.ipinfo.headerHost'],
+    use_admin_menu: true,
+    use_plural_view_names: true,
+    // load_model_population: 'asset',
+    // load_multiple_model_population: 'asset',
+    use_full_data: true,
+    extname: 'periodicjs.ext.dblogger'
+  };
+  let override = {
+    // create_item: [setCronFilePath, assetController.multiupload, assetController.create_assets_from_files, createCrons],
+    // create_index: [cron_create_index],
+    // delete_item: [deleteCron],
+    update_item: [
+      function(req, res, next) {
+        req.body.runtime_options = JSON.parse(req.body.runtime_options || '{}');
+        next();
+      },
+      CoreController.save_revision,
+      CoreController.controller_update(cronSettings)
+    ]
+  };
+  cronSettings.override = override;
+  let dbloggerController = CoreController.controller_routes(cronSettings);
 
-    // let asyncadminController = resources.app.controller.extension.asyncadmin;
-    // if (asyncadminController) {
-    //     dbloggerController.router.post('/dblogger/:id/edit', asyncadminController.admin.fixCodeMirrorSubmit, CoreController.save_revision, dbloggerController.update);
-    // }
-    return dbloggerController;
+  // let asyncadminController = resources.app.controller.extension.asyncadmin;
+  // if (asyncadminController) {
+  //     dbloggerController.router.post('/dblogger/:id/edit', asyncadminController.admin.fixCodeMirrorSubmit, CoreController.save_revision, dbloggerController.update);
+  // }
+  return dbloggerController;
 };
