7d382fd87b0296d7587de1b70d2fbc713860b01c
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-04-03 22:11:41 -0400

    working db logs

--- router/index.js
+++ router/index.js
@@ -6,7 +6,7 @@ module.exports = function(periodic) {
   const ExtensionRouter = periodic.express.Router();
   const DBLoggerRouter = require(path.resolve(__dirname, './dblogger'))(periodic);
   const reUtilPath = path.join(__dirname, '../../../node_modules/periodicjs.ext.reactadmin/utility/locals.js');
-  let reactadmin = {manifest_prefix:'/r-admin'};
+  let reactadmin = { manifest_prefix: '/r-admin' };
 
   ExtensionRouter.use(`/${periodic.app.locals.adminPath}/content`, DBLoggerRouter);
 
@@ -17,13 +17,13 @@ module.exports = function(periodic) {
   // }
 
   for (var x in periodic.settings.extconf.extensions) {
-		if (periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.reactadmin') {
+    if (periodic.settings.extconf.extensions[x].name === 'periodicjs.ext.reactadmin') {
       const reactadminUtil = require(reUtilPath)(periodic).app.locals.extension.reactadmin;
       const oauth2authController = periodic.app.controller.extension.oauth2server.auth;
       reactadmin = reactadminUtil;
-      ExtensionRouter.use(`${reactadmin.route_prefix}/extension/dblogger`, oauth2authController.ensureApiAuthenticated, DBLoggerRouter);
-		}
-	}
+      ExtensionRouter.use(`${reactadmin.manifest_prefix}extension/dblogger`, oauth2authController.ensureApiAuthenticated, DBLoggerRouter);
+    }
+  }
 
   return ExtensionRouter;
 };
