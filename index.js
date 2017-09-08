9080be9a18a7e89d07da46f95b64cf29ce364f0d
yaw joseph etse <yaw.etse@gmail.com>
2017-09-08 15:55:22 -0400

    attached logger

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
 const periodic = require('periodicjs');
+const utilities = require('./utilities');
 
 module.exports = () => {
+  periodic.status.on('configuration-complete', (status) => {
+    const dbloggerExtSettings = periodic.settings.extensions[ 'periodicjs.ext.dblogger' ];
+    periodic.logger.add(utilities.winston.coreDataWinstonLogger, dbloggerExtSettings);
+  });
   return Promise.resolve(true);
-}
+};
